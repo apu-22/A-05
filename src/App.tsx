30b70de272d1a5c310acb2b8c191c3e77ab81f0e
Apu Rayhan <mdapurayhan1729@gmail.com>
2026-09-13 22:21:31 +0600

    Add fotter section

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,27 @@ import Navbar from "./components/Navbar";
 import TechnolyCard from "./components/tech/TechnolyCard";
 import { Suspense } from "react";
 import type { ItechType } from "./types/techType";
+import Footer from "./components/Footer";
 
-const techDataFetch= async():Promise<ItechType[]> =>{
+const techDataFetch = async (): Promise<ItechType[]> => {
   const res = await fetch("/technologies.json");
   const data = await res.json();
   return data;
 }
 
 function App() {
-  const techData= techDataFetch();
+  const techData = techDataFetch();
   return (
     <>
       <Navbar />
       <Banner />
       <Suspense fallback={<h2>Loading...</h2>}>
-      <TechnolyCard techData={techData}/>
+        <TechnolyCard techData={techData} />
       </Suspense>
+      <Footer />
     </>
   );
 }
 
 export default App;
+
